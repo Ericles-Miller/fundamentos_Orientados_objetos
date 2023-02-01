47836d0fdf4e43298ba2f12fd8f85a6a2a832332
Ericles-Miller <ericlesmiller15@gmail.com>
2023-02-01 09:47:28 -0300

    make exercicies in c#

---
+++ CreateClass/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+namespace MyApp
+{
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello world!");
+    }
+
+  }
+}
+
+class Customer {
+  string name;
+}
